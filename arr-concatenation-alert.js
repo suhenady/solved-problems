baf57d86a531604f4a86b6d14bd81f3d8a4b5ffe
Jewel Addy <102772399+suhenady@users.noreply.github.com>
2022-10-07 20:02:40 -0400

    first commit with array concatenation

---
+++ arr-concatenation-alert.js
@@ -0,0 +1,5 @@
+//Declare a variable, assign it an array of letters, combine the letters into one word, and alert it 
+
+let arrOfLetters = ['L', 'O', 'V', 'E'];
+
+alert(arrOfLetters[0] + arrOfLetters[1] + arrOfLetters [2] + arrOfLetters[3])
