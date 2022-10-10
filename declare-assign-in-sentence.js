b0e1268115ff8d311e7a9eb0729391cab99a8151
Jewel Addy <102772399+suhenady@users.noreply.github.com>
2022-10-10 10:49:26 -0400

    first commit

---
+++ declare-assign-in-sentence.js
@@ -0,0 +1,7 @@
+// Declare a variable and assign it to a sentance that is a positive affirmation. Print the affirmation to the console multiple times using a method
+
+let feeling = 'happy'
+
+let posAffirmation = `I feel accomplished and ${feeling}. `
+
+console.log(posAffirmation.repeat(4))
